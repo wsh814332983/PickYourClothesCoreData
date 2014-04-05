b69d0306ca7444f11e57f75ad610aba5784d46f9
Tianxiang Zhang <tz534@nyu.edu>
2014-04-04 20:40:35 -0400

    Initial Commit

---
+++ PickYourClothesCoreDataTests/PickYourClothesCoreDataTests.m
@@ -0,0 +1,34 @@
+//
+//  PickYourClothesCoreDataTests.m
+//  PickYourClothesCoreDataTests
+//
+//  Created by tianxiang zhang on 4/4/14.
+//  Copyright (c) 2014 tianxiang zhang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PickYourClothesCoreDataTests : XCTestCase
+
+@end
+
+@implementation PickYourClothesCoreDataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
