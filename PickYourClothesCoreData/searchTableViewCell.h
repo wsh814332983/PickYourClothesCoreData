3be898673725895b8b7a16927873d7dcc4515963
Tianxiang Zhang <tz534@nyu.edu>
2014-05-11 11:14:54 -0400

    Revert ac6505b..4a57954
    This rolls back to commit ac6505b3302ae9faec81f51c40301c302e5aa823.

---
+++ PickYourClothesCoreData/searchTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  searchTableViewCell.h
+//  PickYourClothesCoreData
+//
+//  Created by tianxiang zhang on 5/6/14.
+//  Copyright (c) 2014 tianxiang zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface searchTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *name;
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+
+@end
---
+++ PickYourClothesCoreData/searchTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  searchTableViewCell.m
+//  PickYourClothesCoreData
+//
+//  Created by tianxiang zhang on 5/6/14.
+//  Copyright (c) 2014 tianxiang zhang. All rights reserved.
+//
+
+#import "searchTableViewCell.h"
+
+@implementation searchTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
